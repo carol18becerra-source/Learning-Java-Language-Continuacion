2fa554732468347c7b6066429b5002f2dfd90f2f
Carolina <carol18becerra@gmail.com>
2026-04-17 14:26:47 +0200

    sentencia switch explicada

--- learning-java-language-continuacion/src/main/java/com/example/App.java
+++ learning-java-language-continuacion/src/main/java/com/example/App.java
@@ -1,10 +1,37 @@
 package com.example;
 
-/**
- * Hello world!
- */
+import java.time.DayOfWeek;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /*
+         * Sentencia switch case. esta sentencia es una alternativa al uso de una
+         * sentencia if-else
+         * cuando haya que evaluar muchas condiciones
+         * 
+         * documentacion de switch case:
+         * https://docs.oracle.com/javase/tutorial/java/nutsandbolts/switch.html
+         */
+
+
+
+        DayOfWeek today = DayOfWeek.FRIDAY;
+
+        switch (today) {
+            case MONDAY: 
+                System.out.println("Vaya! Se hacen eternos los lunes"); 
+                break;
+            case TUESDAY:
+                System.out.println("Es martes");
+                break;
+            case FRIDAY:
+                System.out.println("Es viernes y el cuerpo lo sabe");
+                break;
+        
+            default:
+                System.out.println("No existe ninguna info para el dia introducido ");
+                break;
+        }
+
     }
 }
